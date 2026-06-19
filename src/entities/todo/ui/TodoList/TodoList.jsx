56cbfde955d3a1015f3ccc74ef514249d4e715d9
Alexey S. <person432@ya.ru>
2026-06-19 18:48:45 +0300

    move files to Feature-Sliced Design structure

---
+++ src/entities/todo/ui/TodoList/TodoList.jsx
@@ -0,0 +1,40 @@
+import { memo, useContext } from "react";
+import TodoItem from "../TodoItem/TodoItem";
+import { TasksContext } from "../../model/TasksContext";
+
+const TodoList = (props) => {
+  const { styles } = props
+
+  const {
+    tasks,
+    filteredTasks,
+  } = useContext(TasksContext);
+
+  const HasTasks = tasks.length > 0;
+  const isEmptyFilteredTasks = filteredTasks?.length === 0;
+
+  if (!HasTasks) {
+    return <div className={styles.emptyMessage}>There are not tasks yet</div>;
+  }
+
+  if (HasTasks && isEmptyFilteredTasks) {
+    return <div className={styles.emptyMessage}>Tasks not found</div>;
+  }
+
+  return (
+    <ul className={styles.list}>
+      {(filteredTasks ?? tasks).map((task) => (
+        <TodoItem
+        className={styles.item}
+        key={task.id}
+        id={task.id}
+        title={task.title}
+        isDone={task.isDone}
+        />
+      ))}
+
+    </ul>
+  );
+};
+
+export default memo(TodoList);
