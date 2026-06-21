35fa39f963c5c6bd1b3feffab489ddadce4de99d
Alexey S. <person432@ya.ru>
2026-06-21 19:12:46 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(( { mode }) => {
+  const isProd = mode === 'production'
+
+  return {
+  base: isProd ? '/todo-react/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+}
 })
